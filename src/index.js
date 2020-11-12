8ec2826c57784d8f6e9318c49aaf199160b6ee59
oskarivilen <oskari.vilen@gmail.com>
2020-11-12 08:41:22 +0200

    misc

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-  
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import QList from "./Qlist";
-  
+
+import Surveylist from "./components/Surveylist"; 
 var destination = document.querySelector("#container")
   
 ReactDOM.render(
     <div>
-        <p>Kerro lemppariruoka</p>
-        <QList/>
-        <p>Kerro lemppariväri</p>
-        <QList/>
-        <p>Kerro henkilöturvatunnus</p>
-        <QList/>
+       <Surveylist/>
     </div>,
     destination
 );
